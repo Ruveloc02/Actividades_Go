94ecce9d0d8d3fe5f3f522cefd7a5ed340460bd2
Carlos Ruiz Velasco Lopez <carlos.ruizvelasco2917@alumnos.udg.mx>
2020-09-30 22:05:05 -0500

    Corregir programa

--- Euler/main.go
+++ Euler/main.go
@@ -3,25 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	var iteraciones int
+	var euler float64
+	var aux float64
 
-	var n float64
-	var i float64
-	var e float64
-	e = 0
-	fmt.Scan(&n)
-	for i = 0; i < n; i++ {
-		var aux float64 = fact(i)
-		e = e + (1 / aux)
-	}
-	fmt.Print(e)
-}
+	fmt.Scan(&iteraciones)
 
-func fact(i float64) float64 {
-	var factorial float64 = i
-	if factorial > 1 {
-		factorial = i * fact(i-1)
-		return factorial
-	} else {
-		return 1
+	for i := 0; i < iteraciones+1; i++ {
+		aux = 1
+		for j := i; j >= 1; j-- {
+			aux = aux * float64(j)
+		}
+		euler = euler + (1 / aux)
 	}
+	fmt.Print(euler)
 }
